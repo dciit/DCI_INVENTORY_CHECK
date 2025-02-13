9150bdbe412639a6d06b5ecfe3be2290368b5f0e
Meawjai <wananuthida.46@gmail.com>
2025-02-13 19:41:20 +0700

    .

--- src/pages/summerizegoods.tsx
+++ src/pages/summerizegoods.tsx
@@ -78,7 +78,7 @@ function SummerizeGoods() {
                     </span>
                     <Input className="border border-black text-lg text-black font-semibold text-start w-60"></Input>
                     <div id="search" className="flex flex-1 justify-end">
-                        <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Blue</button>
+                    <button type="button" className="focus:outline-none text-white bg-[#FFEEBB] hover:bg-[#0057920 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Search</button>
                     </div>
                 </div>
             </div>
